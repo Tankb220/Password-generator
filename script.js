322ada58cb1376592a43cffb895003d069711029
James Belcher <130000791+Tankb220@users.noreply.github.com>
2023-05-01 15:27:22 -0500

    added if statements and connected length to them

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword (){ 
   // your code here
 // length 
-  var length = prompt("Please enter the length of you password between 8 and 128.");
+  var length = prompt("Please enter the length of your password between 8 and 128 characters.");
+  if (length < 8 || length >128){
+    alert("Length must be between 8-128 characters")
+  }
   //select the characters
-  var chartype = window.prompt("Do you want to use lower case letters?");
-  var chartype = window.prompt("Do you want to use any special characters?");
-  var chartypee = window.prompt("Do you want to use UPPERCASE letters?");
-  var chartype = window.prompt("Do you want to use numbers?");
+  if (length >= 8 && length <= 128){
+  var chartype = window.confirm("Do you want to use lower case letters?");
+  var chartype = window.confirm("Do you want to use any special characters?");
+  var chartypee = window.confirm("Do you want to use UPPERCASE letters?");
+  var chartype = window.confirm("Do you want to use numbers?");
+  }
 
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var symbols = "!@#$%^&*()-_+=.,:;<>?[]`{}|~"; 
@@ -21,10 +26,13 @@ function generatePassword (){
 
   var random = Math.floor(Math.random() * symbols.length);
 
+  var random = Math.floor(Math.random() * uppercase.length);
+
+  var random = Math.floor(Math.random() * numbers.length);
+
+
+  
   console.log("RANDOM", random, lowercase[random], symbols[random], uppercase[random], numbers[random]);
-
-
-  return "";
 
 }
 // Write password to the #password input
