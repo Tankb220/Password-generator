6e1a361fb0c3ffcf50e91a51b301fc8e0fa3b8a1
James Belcher <130000791+Tankb220@users.noreply.github.com>
2023-05-03 18:02:17 -0500

    made changes to the selection area to make it work with a for loop and got to generate random passwords

--- script.js
+++ script.js
@@ -3,44 +3,50 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword (){ 
   // your code here
+// character and letters to be used
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var symbol = "!@#$%^&*()-_+=.,:;<>?[]`{}|~"; 
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var number = "0123456789";
+  var characterSet = "";
 // length 
-  var length = prompt("Please enter the length of your password between 8 and 128 characters.");
+   length = prompt("Please enter the length of your password between 8 and 128 characters.");
   if (length < 8 || length >128){
     alert("Length must be between 8-128 characters")
+    return;
   }
   //select the characters
-  if (length >= 8 && length <= 128){
-  var clower = window.confirm("Do you want to use lower case letters?");
-  var csymbol = window.confirm("Do you want to use any special characters?");
-  var cupper = window.confirm("Do you want to use UPPERCASE letters?");
-  var cnumber = window.confirm("Do you want to use numbers?");
-  }
+   clower =confirm("Do you want to use lower case letters?");
+   csymbol = confirm("Do you want to use any special characters?");
+   cupper = confirm("Do you want to use UPPERCASE letters?");
+   cnumber = confirm("Do you want to use numbers?");
   
   if (cupper != true && clower != true && csymbol != true && cnumber != true){
-    alert("You must select at least one character type!!!!")
+    alert("You must select at least one character type!!!!");
+  }
+  if (clower) {
+    characterSet += 'lower';
+  }
+  if (cupper) {
+    characterSet += upper;
+  }
+ if (csymbol) {
+  characterSet += symbol;
+ }
+ if (cnumber){
+  characterSet += number;
+ }
+
+  // once selected randomized
+  
+  for (var i = 0; i < length; i++){
+  var random = [Math.floor(Math.random() * characterSet.length)];
+    password += characterSet[random];
   }
 
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var symbols = "!@#$%^&*()-_+=.,:;<>?[]`{}|~"; 
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numbers = "0123456789";
-
-
-  var random = Math.floor(Math.random() * lowercase.length);
-
-  var random = Math.floor(Math.random() * symbols.length);
-
-  var random = Math.floor(Math.random() * uppercase.length);
-
-  var random = Math.floor(Math.random() * numbers.length);
-
-
   
-  console.log("RANDOM", random, lowercase[random]);
-  console.log("RANDOM", random,symbols[random]);
 
-
-  return "";
+  return password;
 
 }
 // Write password to the #password input
